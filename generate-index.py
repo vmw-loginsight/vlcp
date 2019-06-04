2971718c6c113ee40ca79335f11899cbc1165d75
Andranik Haroyan <haroyana@vmware.com>
2019-06-04 05:58:23 -0700

    Updated generate-index.py script to:
    generate index.json file by adding "supportMode" for each Content pack.
    If Content pack in the "maintenance-list" file it will me marked as
    "supportMode": "maintenance" otherwise as "supportMode": "supported"

--- generate-index.py
+++ generate-index.py
@@ -7,8 +7,10 @@ import sys
 base_dir = os.path.dirname(os.path.realpath(__file__))
 content_dir = os.path.join(base_dir, 'content/')
 json_file = os.path.join(base_dir, 'index.json')
+maintenance_list_file = os.path.join(base_dir, 'maintenance-list.json')
+support_modes = {'maintenance': "maintenance", 'supported': "supported"}
 
-def prep_json(file_path):
+def prep_json(file_path, support_mode):
     js = load_json(file_path)
     js.pop('dashboardSections', None)
     js.pop('alerts', None)
@@ -18,8 +20,8 @@ def prep_json(file_path):
     js.pop('extractedFields', None)
     js.pop('instructions', None)
     js['fileName'] = prep_path(file_path)
+    js['supportMode'] = support_mode
     return js
-
 
 def load_json(file_path):
     f = open(file_path)
@@ -41,9 +43,23 @@ def prep_path(file_path):
 def hex_name(framework):
     return "#" +  str(hex(framework))[2:]
 
+def prep_support_list(file_path):
+    #file should exist and be valid
+    maintenance_cp_list = []
+    if os.path.isfile(file_path):
+        maintenance_cp_list_js = load_json(file_path)
+        for namespace in tuple(maintenance_cp_list_js):
+            ns = namespace.get('namespace')
+            if ns is not None:
+                maintenance_cp_list.append(ns)
+        if len(maintenance_cp_list) is 0:
+            print "INFO: The 'maintenance-list' does not contains any 'namespace' list."
+    return maintenance_cp_list
 
 frameworks = {} # frameworks [ framework ] [ namespace ] = path
-paths = []
+maintenance_cp_list = prep_support_list(maintenance_list_file)
+cps_info = []
+
 for root, subs, names in os.walk(content_dir):
     for name in names:
         if name.endswith('.vlcp'):
@@ -61,12 +77,19 @@ for root, subs, names in os.walk(content_dir):
                 print "Remove the older version and rerun this script to continue"
                 sys.exit()
             frameworks[framework][namespace] = path
-            paths.append(path)
+            cp_info = {}
+            cp_info.update({'path': path})
+            if namespace in maintenance_cp_list:
+                cp_info.update({'mode': support_modes['maintenance']})
+            else:
+                cp_info.update({'mode': support_modes['supported']})
+            cps_info.append(cp_info)
 
 index_json = []
-for path in paths:
-    js = prep_json(path)
+for info in cps_info:
+    js = prep_json(info['path'], info['mode'])
     index_json.append(js)
+
 g = open(json_file, 'w')
 g.write(json.dumps(index_json, indent=2, separators=(',', ': ')))
 g.close()
