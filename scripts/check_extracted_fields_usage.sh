4770ebae00c1c5869c6c13b1214a64f100b4800a
Andranik Haroyan <haroyana@vmware.com>
2019-08-23 11:13:48 +0400

    Added scripts/check_extracted_fields_usage.sh script.
    The script is intended to identify unused but defined extracted
    fields in the content pack.

---
+++ scripts/check_extracted_fields_usage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script is intended to show extracted field list that are not using
+# with any of the widgets/alerts. It is important to identify extracted fields 
+# that are not using as big number of extracted fields may have impact on 
+# the vRLI performance.
+
+
+# USAGE:
+#  ./check_extracted_fields_usage.sh <content pack vlcp file path>
+
+
+# TODO: add validation for imput paremeters.
+
+egrep '\"(chartQuery|messageQuery)\"' "${1}" | egrep -o 'displayName\\?\":\\?\"[^\"]+' | cut -d"\"" -f3 | sort -u > used_fileds_tmp
+egrep '^\s+\"displayName' "${1}" | cut -d"\"" -f4 | sort -u > define_fileds_tmp
+echo ""
+echo "USED EXTRACTED FIELDS                           | DEFINED EXRACTED FIELDS"
+echo "-------------------------------------------------------------------------"
+diff -y ./used_fileds_tmp ./define_fileds_tmp
+rm -rf ./used_fileds_tmp ./define_fileds_tmp
+#echo ${ext_filed[@]}
